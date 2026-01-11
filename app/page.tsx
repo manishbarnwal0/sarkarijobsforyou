1b2824ba231ed92d037a50aab2e532202ebba748
manishbarnwal0 <manishkrbarnwal0@gmail.com>
2026-01-11 09:19:42 +0530

    feat: Render basic homepage content
    Adds a simple `HomePage` component to `app/page.tsx`. This confirms that Next.js is correctly rendering the root page of the application.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Homepage Rendering Works</h1>
+      <p>If you see this, Next.js rendering is OK.</p>
+    </main>
+  );
+}
